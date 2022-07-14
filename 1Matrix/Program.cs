57f1f26a160dc0f2429ee604e888ab8f8df1e101
Zhunus <samsungus071@gmail.com>
2022-07-14 14:59:47 +0600

    add TwoDimenArray

---
+++ 1Matrix/Program.cs
@@ -0,0 +1,32 @@
+﻿// string[,] table = new string[2, 5];
+// String.Empty
+
+
+int[,] matrix = new int[3, 4];
+
+void PrintArray(int[,] matx)
+{
+    for (int i = 0; i < matx.GetLength(0); i++)
+    {
+        for (int j = 0; j < matx.GetLength(1); j++)
+        {
+            Console.Write($"{matx[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matx)
+{
+    for (int i = 0; i < matx.GetLength(0); i++)
+    {
+        for (int j = 0; j < matx.GetLength(1); j++)
+        {
+            matx[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
